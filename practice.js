3b61cc03f8ae82fde3ee3e588689190a51112f5f
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2024-11-13 15:34:26 -0500

    Initial commit

---
+++ practice.js
@@ -0,0 +1,97 @@
+// // function sum_array(array) {
+// //     let new_array = []
+// //     let count = 0
+// //     for (i = 0; i < array.length; i ++) {
+// //         if (array[i][0].startsWith("a")) {
+// //             new_array.push(array[i])
+// //             count += 1
+// //         }
+// //     }
+// //     return new_array
+// // }
+
+// // // array = ["apple", "banana", "apricot"]
+// // // console.log(sum_array(array))
+
+// // function true_number(array, number) {
+// //     for (i = 0; i < array.length; i ++) {
+// //         if (number == array[i]) {
+// //             return i
+// //         }
+// // }
+// // //     return -1
+// // // }
+// // console.log(true_number([1, 7, 4, 9, 8], 7))
+
+
+
+// //     let blue = "blue"
+// //     let green = "green"
+// //     let yellow = "yellow"
+// //     let array = []
+
+// //     for (i = 0; i < 100; i ++) {
+// //         if (i <= 50 ) {
+// //             array.push(blue)
+// //         }
+// //         if (i <=75) {
+// //             array.push(green)
+// //         }
+// //         if (i <= 100) {
+// //             array.push(yellow)
+// //         }
+// //     }
+
+// //   var item = array[Math.floor(Math.random()*array.length)];
+// //   console.log(item)
+
+
+// // function sum_array(array, number) {
+// //     let new_array = []
+// //     for (i = 0; i < array.length; i ++) {
+// //         if (array[i] == number) {
+// //             return i
+// //         } else {
+// //             new_array.push(array[i])
+// //         }
+// //     }
+// //     return new_array
+// // }
+
+// // console.log(sum_array([1, 5, 5, 4, 3, 4], 7))
+
+
+// // function starts_with(array) {
+// //     let new_array = []
+// //     for (i = 0; i < array.length; i ++) {
+// //         if (array[i][0].startsWith("a")) {
+// //             new_array.push(array[i])
+// //         }
+// //     }
+// //     return new_array
+// // }
+ 
+// // console.log(starts_with(["apple", "bottom", "jeans", "apricot"]))
+
+// let blue = "blue"
+// let green = "green"
+// let yellow = "yellow"
+// let array = []
+
+// for (i = 0; i < 100; i ++) {
+//     if (i <= 30) {
+//         array.push(blue)
+//     } else {
+//         if (i <= 60) {
+//             array.push(green)
+//         } else {
+//             if ( i <= 100) {
+//                 array.push(yellow)
+//             }
+//         }
+//     }
+    
+// }
+
+// var item = array[Math.floor(Math.random()*array.length)];
+// console.log(item)
