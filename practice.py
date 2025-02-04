4c357ecf2c281df7b888ef2c9977a79af8d298b4
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2025-02-03 19:13:59 -0500

    magic number problem

--- practice.py
+++ practice.py
@@ -1,6 +1,21 @@
-pi = 3.141592653589793238462643383279502884197
-print(f"{pi:.2f}")
-num = pi * 10
-print(f"{num:.2f}")
-num2 = pi * 10000
-print(f"{num2:,.2f}")
+# pi = 3.141592653589793238462643383279502884197
+# print(f"{pi:.2f}")
+# num = pi * 10
+# print(f"{num:.2f}")
+# num2 = pi * 10000
+# print(f"{num2:,.2f}")
+
+#Magic Date Problem
+
+month = int(input("Enter a month(numeric form)"))
+day = int(input("Enter a day(numeric form)"))
+year = int(input("Enter a year(last two digits"))
+
+print(f"{month}/{day}/{year}")
+
+if month * day == year:
+    print("The date is magic")
+else: 
+    print("The date is not magic")
+
+
