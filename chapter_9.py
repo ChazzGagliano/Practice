5e0e63ebe8a6ee39977cc25d112d02a7f5f821bd
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2025-03-17 12:49:25 -0400

    8 Names and Email Addresses

--- chapter_9.py
+++ chapter_9.py
@@ -33,77 +33,157 @@
 #     print(f"Meeting time: {meeting_time[user_input]}")
 
 #2 Capital Quiz
+# def main():
+#    sc = {
+#     "Alabama": "Montgomery",
+#     "Alaska": "Juneau",
+#     "Arizona": "Phoenix",
+#     "Arkansas": "Little Rock",
+#     "California": "Sacramento",
+#     "Colorado": "Denver",
+#     "Connecticut": "Hartford",
+#     "Delaware": "Dover",
+#     "Florida": "Tallahassee",
+#     "Georgia": "Atlanta",
+#     "Hawaii": "Honolulu",
+#     "Idaho": "Boise",
+#     "Illinois": "Springfield",
+#     "Indiana": "Indianapolis",
+#     "Iowa": "Des Moines",
+#     "Kansas": "Topeka",
+#     "Kentucky": "Frankfort",
+#     "Louisiana": "Baton Rouge",
+#     "Maine": "Augusta",
+#     "Maryland": "Annapolis",
+#     "Massachusetts": "Boston",
+#     "Michigan": "Lansing",
+#     "Minnesota": "Saint Paul",
+#     "Mississippi": "Jackson",
+#     "Missouri": "Jefferson City",
+#     "Montana": "Helena",
+#     "Nebraska": "Lincoln",
+#     "Nevada": "Carson City",
+#     "New Hampshire": "Concord",
+#     "New Jersey": "Trenton",
+#     "New Mexico": "Santa Fe",
+#     "New York": "Albany",
+#     "North Carolina": "Raleigh",
+#     "North Dakota": "Bismarck",
+#     "Ohio": "Columbus",
+#     "Oklahoma": "Oklahoma City",
+#     "Oregon": "Salem",
+#     "Pennsylvania": "Harrisburg",
+#     "Rhode Island": "Providence",
+#     "South Carolina": "Columbia",
+#     "South Dakota": "Pierre",
+#     "Tennessee": "Nashville",
+#     "Texas": "Austin",
+#     "Utah": "Salt Lake City",
+#     "Vermont": "Montpelier",
+#     "Virginia": "Richmond",
+#     "Washington": "Olympia",
+#     "West Virginia": "Charleston",
+#     "Wisconsin": "Madison",
+#     "Wyoming": "Cheyenne"
+# }
+
+#    state_capitals(sc)
+
+# def state_capitals(sc):
+#     correct = 0
+#     incorrect = 0
+#     while True:
+#         for state,capital in sc.items():
+#             print(f"Enter the capital for {state}")
+#             user_input = input().lower()
+#             if user_input == "end":
+#                 print(f"You got {correct} correct and {incorrect} incorrect. ")
+#                 return
+#             elif user_input == capital.lower():
+#                 print("correct!")
+#                 correct += 1
+#             else:
+#                 print("incorrect!")
+#                 incorrect += 1
+# main()
+
+#8 Names and Email Addresses
+import pickle
+
+def load_data():
+    try:
+        with open("contacts.pkl", "rb") as file:
+            return pickle.load(file)
+    except FileNotFoundError:
+        print("No existing contact file found. Starting with an empty list.")
+        return {}
+
+def save_contacts(contacts):
+    with open("contacts.pkl", "wb") as file:
+        pickle.dump(contacts, file)
+
+
+def data_menu():
+    print("Menu:")
+    print("1. Search email address: ")
+    print("2. Create new contact: ")
+    print("3. Update an email address: ")
+    print("4. Delete a contact: ")
+    print("Exit")
+
+def search_contact(contacts):
+    name = input("Enter a name: ")
+    if name not in contacts:
+        print("Contact doesn't exist.")
+    else:
+        print(f"{name}'s email: {contacts.get(name)}")
+
+def create_contact(contacts):
+    name = input("Enter a name: ")
+    if name not in contacts:
+        email = input("Enter a new email: ")
+        contacts[name] = email
+    else:
+        print("This contact already exists.")
+
+def update_contact(contacts):
+    name = input("Enter a name to update: ")
+    if name not in contacts:
+        print("Contact doesn't exist")
+    else:
+        email = input("Enter the new email: ")
+        contacts[name] = email
+        print("Email updated.")
+
+def delete_contact(contacts):
+    name = input("Enter a name: ")
+    if name not in contacts:
+        print("Contact doesn't exist.")
+    else:
+        del contacts[name]
+        print("Contact deleted")
+    
 
 def main():
-   sc = {
-    "Alabama": "Montgomery",
-    "Alaska": "Juneau",
-    "Arizona": "Phoenix",
-    "Arkansas": "Little Rock",
-    "California": "Sacramento",
-    "Colorado": "Denver",
-    "Connecticut": "Hartford",
-    "Delaware": "Dover",
-    "Florida": "Tallahassee",
-    "Georgia": "Atlanta",
-    "Hawaii": "Honolulu",
-    "Idaho": "Boise",
-    "Illinois": "Springfield",
-    "Indiana": "Indianapolis",
-    "Iowa": "Des Moines",
-    "Kansas": "Topeka",
-    "Kentucky": "Frankfort",
-    "Louisiana": "Baton Rouge",
-    "Maine": "Augusta",
-    "Maryland": "Annapolis",
-    "Massachusetts": "Boston",
-    "Michigan": "Lansing",
-    "Minnesota": "Saint Paul",
-    "Mississippi": "Jackson",
-    "Missouri": "Jefferson City",
-    "Montana": "Helena",
-    "Nebraska": "Lincoln",
-    "Nevada": "Carson City",
-    "New Hampshire": "Concord",
-    "New Jersey": "Trenton",
-    "New Mexico": "Santa Fe",
-    "New York": "Albany",
-    "North Carolina": "Raleigh",
-    "North Dakota": "Bismarck",
-    "Ohio": "Columbus",
-    "Oklahoma": "Oklahoma City",
-    "Oregon": "Salem",
-    "Pennsylvania": "Harrisburg",
-    "Rhode Island": "Providence",
-    "South Carolina": "Columbia",
-    "South Dakota": "Pierre",
-    "Tennessee": "Nashville",
-    "Texas": "Austin",
-    "Utah": "Salt Lake City",
-    "Vermont": "Montpelier",
-    "Virginia": "Richmond",
-    "Washington": "Olympia",
-    "West Virginia": "Charleston",
-    "Wisconsin": "Madison",
-    "Wyoming": "Cheyenne"
-}
-
-   state_capitals(sc)
-
-def state_capitals(sc):
-    correct = 0
-    incorrect = 0
+    contacts = load_data()
     while True:
-        for state,capital in sc.items():
-            print(f"Enter the capital for {state}")
-            user_input = input().lower()
-            if user_input == "end":
-                print(f"You got {correct} correct and {incorrect} incorrect. ")
-                return
-            elif user_input == capital.lower():
-                print("correct!")
-                correct += 1
-            else:
-                print("incorrect!")
-                incorrect += 1
+        data_menu()
+        option = input("Enter option 1-4(enter exit to leave): ").lower()
+        if option == "exit":
+            save_contacts(contacts)
+            print("Data saved. Bye!")
+            break
+        elif option == "1":
+            search_contact(contacts)
+        elif option == "2":
+            create_contact(contacts)
+        elif option == "3":
+            update_contact(contacts)
+        elif option == "4":
+            delete_contact(contacts)
+        else:
+            print("invalid option.. Try again.")
+
 main()
+
+
