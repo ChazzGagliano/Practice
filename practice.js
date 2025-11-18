598be79aa6edda32ee1549931221fa749b863254
Chazz Gagliano <127808184+ChazzGagliano@users.noreply.github.com>
2025-11-18 17:04:28 -0500

    javascript sum practice

--- practice.js
+++ practice.js
@@ -392,3 +392,13 @@
     // }
 
     // console.log(array_sum([1, 2, 3, 4]))
+
+function sum_array(array) {
+    let sum = 0
+        for (let i = 0; i < array.length; i ++){
+            sum += array[i]
+        }
+    return sum
+}
+
+console.log(sum_array([1, 2, 3, 4]))
